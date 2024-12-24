b19917fc30f62e569aa7d777e0c516eba8954747
AZ3601 <1840219745@qq.com>
2024-12-24 19:53:40 +0800

    third commit

--- app/src/main/java/com/example/txtreader/ApiClient.java
+++ app/src/main/java/com/example/txtreader/ApiClient.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+	//重新启动ngork后需将新的公网地址复制到这里
     private static final String BASE_URL = "https://ebda-58-249-112-27.ngrok-free.app";
     private static Retrofit retrofit = null;
 
